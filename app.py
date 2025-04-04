aff88f95fc47b871a0c879c8c33023edce58a95b
gomex500 <gomezfreddy886@gmail.com>
2025-04-03 20:34:10 -0600

    prueba 1 interaccion exitosa

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
-
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def hello():
     return 'Hello bitch!'
 
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message: '+msg)
+    send(msg, broadcast=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
